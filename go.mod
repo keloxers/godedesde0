330a2ef5135bdc8fb122c5f872650ee78ef7b3af
Miguel Mendez <keloxers@gmail.com>
2023-06-14 09:43:30 -0300

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/keloxers/godedesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/keloxers/godedesde0/variables"
+
+func main() {
+
+	variables.MuestroEnteros()
+
+}
