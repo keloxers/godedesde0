cfeaed980c66cf545be1fc355366e22856a04949
Miguel Mendez <keloxers@gmail.com>
2023-06-22 10:57:56 -0300

    Siguiente commit

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import "github.com/keloxers/godedesde0/variables"
+import (
+	"fmt"
+
+	"github.com/keloxers/godedesde0/variables"
+)
 
 func main() {
 
-	variables.MuestroEnteros()
+	// variables.MuestroEnteros()
+	//variables.RestoVariables()
+
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println((estado))
+	fmt.Println(texto)
 
 }
